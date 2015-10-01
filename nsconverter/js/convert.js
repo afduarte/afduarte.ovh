37e78a9a9fc84da786e4700fa24a6a462d989c71
Antero Duarte <mail@afduarte.ovh>
2015-10-01 13:34:18 +0100

    ***

---
+++ nsconverter/js/convert.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    var input = $('#userNum').value;
+    var convTo = $('input[name=convTo]');
+
+    console.log(input);
+    console.log(convTo);
+
+    if (convTo===toBin){
+
+        console.log("Convert to binary");
+
+    }
+
+});
+
+function toDec{
+    var input = $('#userNum').value;
+}
+
