745861f93522d12520ce18f01d1b77d21678ac1c
Antero Duarte <mail@afduarte.ovh>
2015-10-01 22:10:10 +0100

    First functional prototype, not live yet

--- nsconverter/js/convert.js
+++ nsconverter/js/convert.js
@@ -1,20 +1,28 @@
-$(document).ready(function(){
+$("input:radio[name=convTo]").on("change click", fromDec);
 
-    var input = $('#userNum').value;
-    var convTo = $('input[name=convTo]');
 
-    console.log(input);
-    console.log(convTo);
-
-    if (convTo===toBin){
-
-        console.log("Convert to binary");
-
+function fromDec(){
+    var convTo = $("input:radio[name=convTo]:checked").attr('id');
+    var input = $('#userNum').val();
+    var result = 0;
+    if (convTo == "toBin"){
+        result = (input >>> 0).toString(2);
+        $('#result').html("("+result+")<sub>2<sub>");
+    } else if (convTo == "toHex"){
+        result = (input >>> 0).toString(16);
+        $('#result').html("("+result+")<sub>16<sub>");
     }
-
-});
-
-function toDec{
-    var input = $('#userNum').value;
 }
 
+function fromDec(){
+    var convTo = $("input:radio[name=convFrom]:checked").attr('id');
+    var input = $('#userBin').val();
+    var result = 0;
+    if (convTo == "toBin"){
+        result = parseInt(input, 2);
+        $('#result').html("("+result+")<sub>10<sub>");
+    } else if (convTo == "toHex"){
+        result = parseInt(input, 16);
+        $('#result').html("("+result+")<sub>10<sub>");
+    }
+}
